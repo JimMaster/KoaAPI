31e916e25e407068b4b1aa7504758361298246b7
cainengjian <cainengjian@f-road.com.cn>
2019-09-27 19:18:45 +0800

    fix bug

--- src/api/login/unionLogin.controller.js
+++ src/api/login/unionLogin.controller.js
@@ -4,11 +4,14 @@ const tools = require('../../utils/tools')
 const config = require('../../config/config')
 exports.unionLogin = async (ctx) => {
     let Response = config.wechat
+    let params = ''
     if (tools.checkUserAgent(ctx.header['user-agent']) == 'WeChat') {
-        let options = { 
-            methods: 'get',
+        for (let i in config.wechat) {
+            params = params + i + '=' + config.wechat[i] + '&'
         }
-        ctx.response.redirect('https://open.weixin.qq.com/connect/oauth2/authorize?' + qs(config.wechat));
+        params = params + '&state=STATE#wechat_redirect'
+        router.get('https://open.weixin.qq.com/connect/oauth2/authorize?' + params);
+        console.log('https://open.weixin.qq.com/connect/oauth2/authorize?' + params)
     }
     if (tools.checkUserAgent(ctx.header['user-agent']) == 'AliPay') {
 
--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
     wechat : {
-        appid: 'wx4c216f094e972e39',
-        secret: '746c40c689b7d3a9a7438089dcd1a919',
+        appid: 'wxdf9df9cd8982c7fb',
+        redirect_uri: 'http://test.candy3015.cn',
         response_type: 'code',
+        // secret: '746c40c689b7d3a9a7438089dcd1a919',
         scope: 'snsapi_base',
-        redirect_uri: 'https://test.candy3015.cn/coupon'
     }
 }

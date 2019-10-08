5027dbad21314cccbc7a4695498a51bca2ec0bf5
cainengjian <cainengjian@f-road.com.cn>
2019-10-08 11:15:46 +0800

    fix bug

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const Koa = require('koa')
 const app = new Koa()
 const config = require('./config/env')
 const cors = require('koa2-cors')
+app.use(cors())
 require('./router')(app)
 require('./config/koa')(app)
 module.exports = app
